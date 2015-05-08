050449aa0156a3735cb78b9b21923f0f1d9a1c0e
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-08 15:15:32 -0600

    add seed data

--- movietime/app/controllers/movies_controller.rb
+++ movietime/app/controllers/movies_controller.rb
@@ -1,2 +1,7 @@
 class MoviesController < ApplicationController
+  def index
+  end
 end
+
+
+#WORKING ON DB SEEDS RIGHT NOW
--- movietime/db/seeds.rb
+++ movietime/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Movie.destroy_all
+Review.destroy_all
+
+i = 1
+10.times do
+  Movie.create(
+    name: "Toy Story #{i}",
+    release_date: Time.at(rand * Time.now.to_i)
+  )
+  i+=1
+end
+
+15.times do
+  movie = Movie.all.sample
+  Review.create(
+    reviewer_name: "bill #{i}",
+    title: movie.name,
+    review_text: "Love this film #{i} times",
+    movie_id: movie.id
+    )
+  i+=1
+end
