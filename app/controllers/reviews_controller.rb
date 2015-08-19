d665524db4561b516575e63b921e98c84c7437bb
niconan <nick.hax@gmail.com>
2015-08-19 15:49:46 -0700

    review creation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
 
   def index
-    @reviews = Review.all
+    @reviews = Review.all.order("review_date DESC")
+    @review = Review.new
 
     respond_to do |format|
       format.html
@@ -9,4 +10,28 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(whitelisted_review_params)
+    @review.review_date = Time.now
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to :back rescue redirect_to reviews_path }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { redirect_to root_path }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def whitelisted_review_params
+    params.require(:review).permit(:title, :reviewer_name, :review_text, :movie_id)
+  end
 end
