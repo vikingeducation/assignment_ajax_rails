725424dc0b0e13ca09208839e24d1f5e09d3b8f9
alokp <alokpra@gmail.com>
2015-09-08 14:04:16 -0700

    refactoring js

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,26 +1,43 @@
 $(document).ready(function(){
-  $.ajax({
-    method: 'GET',
-    url: 'reviews.json',
-    dataType: 'json',
-    contentType: 'application/json',
-    success: function(json){
-      insertReviewIntoTable(json);
-    }
-  });
+  populateReviews.run();
 });
 
-var insertReviewIntoTable = function(json){
-  for (var i = 0; i < json.length; i++) {
-    // console.log(json[i]);
-    var name = json[i].reviewer_name;
-    var title = json[i].title;
-    var text = json[i].text;
-    var movie_id = json[i].movie_id;
-    var $review = $("<tr id='" + json[i].id + "'>").appendTo('#reviews-table');
-    $review.append('<td>'+name+'</td>');
-    $review.append('<td>'+title+'</td>');
-    $review.append('<td>'+movie_id+'</td>');
-    $review.append('<td>'+text+'</td>');
-  }
-};
+var populateReviews = (function(){
+
+  var useJSONObjectToPopulateTable = function(){
+    $.ajax({
+      method: 'GET',
+      url: 'reviews.json',
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function(json){
+        insertReviewIntoTable(json);
+      }
+    });
+  };
+
+  var insertReviewIntoTable = function(json){
+    for (var i = 0; i < json.length; i++) {
+      // console.log(json[i]);
+      var name = json[i].reviewer_name;
+      var title = json[i].title;
+      var text = json[i].text;
+      var movie_id = json[i].movie_id;
+      var $review = $("<tr id='" + json[i].id + "'>").appendTo('#reviews-table');
+      $review.append('<td>'+name+'</td>');
+      $review.append('<td>'+title+'</td>');
+      $review.append('<td>'+movie_id+'</td>');
+      $review.append('<td>'+text+'</td>');
+    }
+  };
+
+  return {
+    run: useJSONObjectToPopulateTable
+  };
+
+})();
+
+
+
+// create reviews module
+// add page specific JS
