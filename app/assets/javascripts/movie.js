e362695f17a337a4fd6ca6bdfd88c357fefdb601
Sam O'Keefe <samok13@gmail.com>
2016-03-18 10:32:44 -0700

    setting up autopopulate w/ajax

---
+++ app/assets/javascripts/movie.js
@@ -0,0 +1,17 @@
+Movies = (function(){
+
+
+
+
+  return{
+
+  };
+})();
+
+$(document).ready(function(){
+   if($('#movies-index').length){
+      alert('Elements w/movie index!')
+   }
+ });
+ 
+
---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,16 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+
+    respond_to do |format|
+      format.html
+      format.json{render :json => @movies, :status => 200}
+    end
+  end
+
+  def show
+    @movie
+  end
+
+end
