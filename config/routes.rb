49f2d4cc40d3411945e91251052990d58c41da2b
Andrew Baik <and.baik@gmail.com>
2016-03-18 12:05:38 -0400

    finished seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :movies
+  resources :reviews
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# ----------------------------------------
+# Database Reset
+# ----------------------------------------
+
+if Rails.env == 'development'
+  puts 'Reseting database'
+
+  Rake::Task['db:migrate:reset'].invoke
+end
+
+# ----------------------------------------
+# Seed Config Vars
+# ----------------------------------------
+
+MULTIPLIER = 10
+
+# ----------------------------------------
+# Seed Config Vars
+# ----------------------------------------
+
+def generate_movies
+  title = Faker::StarWars.character
+  release_date = Date.today - rand(100)
+  Movie.create(title: title, release_date: release_date)
+end
+
+def generate_reviews
+  all_movies = Movie.all
+
+  all_movies.each do |m|
+    reviewer_name = Faker::Name.name
+    title = Faker::Hipster.word
+    review_text = Faker::Hipster.paragraph
+    review_date = Date.today - rand(100)
+
+    3.times do
+      Review.create(
+        reviewer_name:reviewer_name,
+        title: title,
+        review_text: review_text,
+        review_date: review_date,
+        movie_id: m.id)
+    end
+  end
+
+end
+
+puts "Generating movies"
+MULTIPLIER.times { generate_movies }
+puts "Generating reviews"
+generate_reviews
+puts "Done~!"
