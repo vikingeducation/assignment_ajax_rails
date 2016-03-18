49151d6545bd2dd478d5602f838483bed9793d2f
koziscool <kozjob@yahoo.com>
2016-03-18 13:36:06 -0500

    dynamically populate movie list

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -1,17 +1,63 @@
+
 Movies = (function(){
 
+  var showMovieList= function() {
+    console.log('show movie list im here');
+    var url = "http://localhost:3000/movies.json";
+    
+    var callback = function(data) {
+      // console.dir(data);
+      var $movieList = $('#movie-list');
 
+      data.forEach( function(elt){
+        var $movieTableRow = $('<tr></tr>')
+        $movieList.prepend($movieTableRow);
+        console.log(elt);
+        var $movieTDElt = $('<td>' + elt.title + '</td><td>' + elt.release_date + '</td>');
+        $movieTableRow.prepend($movieTDElt);
+      })
+    };
 
+    var errorCallback = function(xhr, status, err){
+      console.log(status);
+      console.log(err);
+    }
 
-  return{
-
+    $.ajax( {
+      url: url, 
+      success: callback,
+      error: errorCallback,
+    } );
   };
+
+  var createMovie= function(e) {
+    console.log('create movie im here');
+    e.preventDefault();
+    console.log(e)
+  };
+
+  var showNoMovies = function() {
+    var $movieList = $('#movie-list');
+    var $noMovies = $('<p>No movies to show</p>');
+    $movieList.append($noMovies);
+  };
+
+  return {
+    showMovieList: showMovieList,
+    createMovie: createMovie,
+    showNoMovies: showNoMovies,
+  };
+
 })();
 
 $(document).ready(function(){
    if($('#movies-index').length){
-      alert('Elements w/movie index!')
+      Movies.showMovieList();
+      $('#movie-form').submit( Movies.createMovie );
+   } else {
+      Movies.showNoMovies();
    }
+
  });
  
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,32 @@ class MoviesController < ApplicationController
     end
   end
 
+  def create
+    @movie = Movie.new
+
+    respond_to do |format|
+      if @movie.save
+
+        # "standard" response
+        format.html { redirect_to @task }
+
+        # Redirect to the SHOW action but as a JS request
+        # which SHOW will handle *as* a JS request
+        format.js { redirect_to @task }
+
+      else
+
+        # normal HTML error handling here.
+        # rerender same template
+        format.html { render :new }
+
+        # for JS, this renders an empty template
+        # (a.k.a.: you get no JavaScript back)
+        format.js { head :none }
+      end
+    end
+  end
+
   def show
     @movie
   end
