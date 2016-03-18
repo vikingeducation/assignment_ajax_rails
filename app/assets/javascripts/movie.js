70522594d87b46e288bf4939f293fd0760aa9237
Sam O'Keefe <samok13@gmail.com>
2016-03-18 15:13:04 -0700

    slight refactor to avoid list reload on add movie--working

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -2,20 +2,16 @@
 Movies = (function(){
 
   var showMovieList= function() {
-    console.log('show movie list im here');
     var url = "http://localhost:3000/movies.json";
     
     var callback = function(data) {
-      // console.dir(data);
       var $movieList = $('#movie-list');
 
       data.forEach( function(elt){
         var $movieTableRow = $('<tr></tr>')
         $movieList.prepend($movieTableRow);
-        // console.log(elt);
         var $movieTDElt = $('<td>' + elt.title + '</td><td>' + elt.release_date + '</td>');
         $movieTableRow.prepend($movieTDElt);
-        //must call add movieToList
       })
     };
 
@@ -32,9 +28,6 @@ Movies = (function(){
   };
 
   var createMovie= function( title ) {
-    console.log('create movie im here again');
-
-    // var data = JSON.stringify( {title: title} );
     var data = {title: title};
 
     $.ajax({
@@ -43,7 +36,7 @@ Movies = (function(){
       data: data,
       dataType: 'json',
       success: function(data){
-        Movies.showMovieList();
+        Movies.addMovieToList(data);
       }, 
       error: function(){
         alert('Could not add your movie.');
@@ -51,15 +44,14 @@ Movies = (function(){
     });
   };
 
-  // var addMovieToList = function(movie){
-  //   console.log(movie);
-  //   var title = movie.title;
-  //   var release_date = movie.release_date;
+  var addMovieToList = function(data){
+    var title = data.title;
+    var release_date = data.release_date;
 
-  //   var $newMovie = $("<tr><td id='movie-title'>" + title + "</td><td id='release_date'>" + release_date + "</td></tr>");
+    var $newMovie = $("<tr><td id='movie-title'>" + data.title + "</td><td id='release_date'>" + data.release_date + "</td></tr>");
 
-  //   $('#movie-list').append($(newMovie));
-  // };
+    $('#movie-list').prepend($newMovie);
+  };
 
   var showNoMovies = function() {
     var $movieList = $('#movie-list');
@@ -71,6 +63,7 @@ Movies = (function(){
     showMovieList: showMovieList,
     createMovie: createMovie,
     showNoMovies: showNoMovies,
+    addMovieToList: addMovieToList
   };
 
 })();
@@ -86,7 +79,6 @@ $(document).ready(function(){
    $('#movie-form').submit( function(e){
         e.preventDefault();
         var movieTitle = $(this).find("#title").val();
-        // console.log('movie title' + movieTitle);
         Movies.createMovie(movieTitle);
     });
  });
