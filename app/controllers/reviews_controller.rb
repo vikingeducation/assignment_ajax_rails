36120816f21dfae1b4db154a7fbb0abf282c18e6
Sia <siakaramalegos@gmail.com>
2016-05-28 17:11:01 -0500

    Renders review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     @reviews = Review.order(:review_date => :desc).includes(:movie)
+    @movies = Movie.order(:title)
   end
 
   def destroy
