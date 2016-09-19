02a55dbdf4890fdce0ed399d0e4b3277e92c3b3d
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-19 11:40:51 -0700

    added ajax funcitonality

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,3 +1,58 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var JS = {};
+
+JS.movies = (function(){
+    var stub = {};
+    stub.init = function() {
+      $.ajax({
+          url: "/movies",
+          method: 'GET',
+          contentType: 'application/json',
+          dataType: 'json',
+          success: function(response){
+            populateTable(response);
+          }
+        }
+      );
+    };
+
+    var populateTable = function(response){
+      $.each(response, function(index, el){
+        var $movieListing = $('<li>').text(el.title);
+        $('ul').append($movieListing);
+      });
+    };
+
+    stub.addMovie = function(movieData){
+      $.ajax({
+        url: "/movies",
+        method: 'POST',
+        data: JSON.stringify({ movie: { title: movieData} }),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function(response){
+          populateTable([response]);
+        },
+        error: function(response){
+          console.log(response);
+        }
+      });
+    };
+
+    stub.addMovieListener = function(){
+      $('input[type="submit"]').on('click', function(e){
+        e.preventDefault();
+        console.log("Hello andur");
+        var movieTitle = $('input[type="text"]').val();
+        stub.addMovie(movieTitle);
+      });
+    };
+
+    return stub;
+})();
+
+$( document ).ready( function(){
+
+  // Attach our listeners after the DOM loads
+  JS.movies.init();
+  JS.movies.addMovieListener();
+});
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @movie = Movie.new
     respond_to do |format|
       format.html
       format.json { render json: @movies }
@@ -11,7 +12,15 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.new(whiteListedMovieParams)
+    if @movie.save
+        render json: @movie
+    end
   end
 
+  private
 
+    def whiteListedMovieParams
+      params.require(:movie).permit(:title)
+    end
 end
