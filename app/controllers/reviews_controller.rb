b0fc832b004b1e0ff28d501b5a4816b81dca6257
cjvirtucio87 <cjv287@gmail.com>
2016-09-19 14:27:06 -0700

    Added dropdown menu for movie reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,9 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
     @review = Review.new
+    @movies = Movie.all
     respond_to do |format|
       format.html
-      format.json { render json: @movies, status: 200 }
     end
   end
 end
