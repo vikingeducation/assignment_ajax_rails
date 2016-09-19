9e93b481ae7e7ed11e83324fd2ac1cd81956d13d
“Matthew <matthew.hinea@gmail.com>
2016-09-19 11:05:04 -0700

    json response working but with no data

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,3 +1,5 @@
+var AJAX = {}
+
 var MoviesView = (function () {
   var _$movieTable;
 
@@ -7,7 +9,34 @@ var MoviesView = (function () {
 
   var _cacheDOM = function() {
     _$movieTable = $('[data-movies-table]').eq();
-    console.log(_$movieTable);
+    console.log("DOM element: " + _$movieTable);
+    AJAX.movies.getMovies(_fillMoviesTable);
   };
+
+  var _fillMoviesTable = function(response) {
+    console.log(response);
+  }
+  
+
+  return {
+    init: init,
+  }
+
+})();
+
+AJAX.movies = (function() {
+
+  var getMovies = function(callback) {
+    $.ajax({
+      url: "/movies.json",
+      method: "GET",
+      dataType: "json",
+      success: callback
+    });
+  }
+
+  return {
+    getMovies: getMovies
+  }
 
 })();
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,8 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     respond_to do |format|
-      format.html { render :index, locals: {movies: @movies} }
-      format.json { render json: @movies, status: 201 }
+      format.html {}
+      format.json { render json: @movies, status: 200 }
     end
   end
 end
