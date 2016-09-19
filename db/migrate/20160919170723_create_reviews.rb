748407fc158bd087582f6a8bf50805382ef5011f
“Matthew <matthew.hinea@gmail.com>
2016-09-19 10:14:49 -0700

    add movie and review models

---
+++ db/migrate/20160919170723_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :reviewer_name
+      t.string :title
+      t.text :review_text
+      t.date :review_date
+      t.integer :movie_id
+      t.timestamps null: false
+    end
+  end
+end
