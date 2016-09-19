952df6572ff458568a44754a8fa2bcc0d7f27fc9
cjvirtucio87 <cjv287@gmail.com>
2016-09-19 14:40:23 -0700

    Added create and destroy actions.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,26 @@ class ReviewsController < ApplicationController
       format.html
     end
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to reviews_path
+  end
+
+  def destroy
+    @review = Review.find_by_id(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:reviewer_name,
+                                   :title,
+                                   :review_text,
+                                   :review_date,
+                                   :movie_id)
+  end
 end
