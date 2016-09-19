11f2070409b3e4e6b27c37f4e3bf325071773f0d
Adrian Mui <adrian.mui@gmail.com>
2016-09-19 13:59:46 -0500

    working on creating movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,24 @@ class MoviesController < ApplicationController
       format.json { render :json => @movies, :status => 201}
     end
   end
+
+  def create
+    @movie = Movie.new(whitelisted_movie_params)
+    @movie.release_date = Time.now
+
+    unless @movie.save
+      
+      render json: {errors: @post.errors.full_messages }, status: :unprocessable_entity
+
+    end
+
+    respond_to :js
+  end
+
+
+  private
+    def whitelisted_movie_params
+      params.require(:movie).permit(:title)
+    end
+
 end
