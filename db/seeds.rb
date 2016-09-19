c87b0c0a892ae8f6be2a79cf0a112103a3c90ddd
cjvirtucio87 <cjv287@gmail.com>
2016-09-19 11:09:14 -0700

    Fixed movie titles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+Review.destroy_all
+Movie.destroy_all
+
 SEED = 10
 
 SEED.times do |idx|
-  movie = Movie.create({title: Faker::Hipster.words(5),
+  movie = Movie.create({title: Faker::Hipster.words(5).join(' '),
                         release_date: Faker::Date.between(50.years.ago,Date.today)})
   5.times do |idx|
-    movie.reviews.create ({reviewer_name: Faker::Hipster.words(2),
+    movie.reviews.create ({reviewer_name: Faker::Hipster.words(2).join(' '),
                           title: Faker::Hipster.sentences(1),
                           review_text: Faker::Hipster.sentences(4),
                           review_date: Faker::Date.between(50.years.ago,Date.today)})
