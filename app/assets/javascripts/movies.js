d5ab195aba5600abdb0a4eade007a6c1a4208407
cjvirtucio87 <cjv287@gmail.com>
2016-09-19 11:23:26 -0700

    Successfully appended new rows.

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -8,19 +8,27 @@ var MoviesView = (function () {
   };
 
   var _cacheDOM = function() {
-    _$movieTable = $('[data-movies-table]').eq();
+    _$movieTable = $('[data-movies-table]');
     console.log("DOM element: " + _$movieTable);
     AJAX.movies.getMovies(_fillMoviesTable);
   };
 
   var _fillMoviesTable = function(response) {
-    console.log(response);
-  }
-  
+    response.forEach(function(row) {
+      var newRow = $('<tr>');
+      var newTitle = $('<td>').text(row.title);
+      var newReleaseDate = $('<td>').text(row.release_date);
+      newRow.append(newTitle);
+      newRow.append(newReleaseDate);
+      newRow.appendTo(_$movieTable);
+      // _$movieTable.append();
+    });
+  };
+
 
   return {
     init: init,
-  }
+  };
 
 })();
 
@@ -33,10 +41,10 @@ AJAX.movies = (function() {
       dataType: "json",
       success: callback
     });
-  }
+  };
 
   return {
     getMovies: getMovies
-  }
+  };
 
 })();
