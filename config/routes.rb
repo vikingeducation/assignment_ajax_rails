dc644dce97823097ca2877206b40dfb65503e26c
schoetlr <srlrs20020@gmail.com>
2016-09-21 14:04:33 -0400

    ajax reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
 
-  resources :movies
+  get 'reviews/index'
 
+  resources :movies
+  resources :reviews
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
