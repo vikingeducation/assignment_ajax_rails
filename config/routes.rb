6d940e60a0aad01e57510769f54e6a4015852ab9
David Watts <davidjeremywatts@gmail.com>
2016-09-29 13:02:12 -0700

    finish review page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
-  # get 'movies', to: 'movies#index'
+  resources :reviews
 end
