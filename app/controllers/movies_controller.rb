e042168e4395dd84ca542e472588cd449c430779
Farruh <ufarruh@gmail.com>
2017-01-05 13:32:26 -0800

    JQuery submit done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.order(created_at: "DESC")
 
     respond_to do |format|
 
@@ -13,19 +13,22 @@ class MoviesController < ApplicationController
 
 
   def create
+    #binding.pry
     @movie = Movie.new(movie_params)
 
-    respond_to do |format|
-      if @movie.save
+    if @movie.save
+
+      @movie.release_date = @movie.created_at
+      @movie.save
+
+      respond_to do |format|
         format.html { redirect_to @movie, notice: "Movie created successfully.." }
-
-        format.json { render :show, status: :created, location: @movie }
-
-      else
-
-        format.html { render :new }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
+        format.json { render json: @movie, status: :created }
       end
+    else
+        format.html { render :new }
+        format.json { render nothing: true, status: 400 }
+
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
   root "movies#index"
-  
+
   resources :movies
+
+  resources :reviews
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
