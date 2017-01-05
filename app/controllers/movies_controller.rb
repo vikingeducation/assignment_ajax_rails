f6e8447637b81965b200292e1564d30ac6eb7550
Farruh <ufarruh@gmail.com>
2017-01-04 18:34:30 -0800

    Movie create with ajax

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,8 +6,32 @@ class MoviesController < ApplicationController
 
 
       format.html
-      
+
       format.json { render json:  @movies, status: 201 }
     end
   end
+
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to @movie, notice: "Movie created successfully.." }
+
+        format.json { render :show, status: :created, location: @movie }
+
+      else
+
+        format.html { render :new }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :release_date)
+    end
+
 end
