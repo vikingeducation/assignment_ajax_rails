7fc2a1907b5fc9aa60e4247b8553ab1f5f8dd8fc
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-16 11:38:54 -0600

    database set up and seeds run

---
+++ smelly_potatoes/db/migrate/20170116172527_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reviews do |t|
+      t.string :reviewer_name
+      t.string :title
+      t.text :review_text
+      t.date :review_date
+      t.integer :movie_id
+
+      t.timestamps
+    end
+    add_index :reviews, :movie_id
+  end
+end
---
+++ smelly_potatoes/db/seeds.rb
@@ -0,0 +1,40 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Destroying movies and reviews"
+
+Movie.destroy_all
+Review.destroy_all
+
+puts "Movies and reviews destroyed"
+
+NUM_MOVIES = 30
+NUM_REVIEWS_PER_MOVIE = 3
+
+
+puts "Creating movies"
+
+NUM_MOVIES.times do
+  movie = Movie.new(title: Faker::Superhero.name, release_date: Faker::Date.between(2.years.ago, Date.today))
+
+
+  puts "Creating reviews for #{movie.title}"
+
+  NUM_REVIEWS_PER_MOVIE.times do
+    movie.reviews << Review.new(
+                                reviewer_name: Faker::Name.name,
+                                title: Faker::Team.name,
+                                review_text: Faker::Lorem.paragraph,
+                                review_date: Faker::Date.between(2.years.ago, Date.today)
+                              )
+  end
+end
+
+puts "Movies and reviews done"
+
