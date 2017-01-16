50ac1f93dc88e32470184bb7f01b1d40b51b22ce
Catherine Kwak <khopsickle@users.noreply.github.com>
2017-01-16 12:01:22 -0700

    JSON form submits via AJAX

--- app/assets/javascripts/json-form.js
+++ app/assets/javascripts/json-form.js
@@ -3,10 +3,41 @@
 var MY_APP = MY_APP || {};
 MY_APP.movies = {};
 
-MY_APP.movies.submit = (function($) {
+MY_APP.movies.create = (function($) {
   var exports = {};
 
-  
+  var attachMovieListener = function() {
+    _ajaxFormListener();
+  };
+
+  var _ajaxFormListener = function() {
+    // is this data-attr ONLY a flag for jQuery?
+    // tradeoff between selecting an ID vs data attr
+
+    // find the form
+    $("form[data-ajaxremote='true']").submit(function(e) {
+      e.preventDefault();
+      var $el = $(e.target);
+      var formData = $el.serializeArray();
+
+      // submit the form via ajax
+      $.ajax({
+        url: $el.attr('action'),
+        method: 'POST',
+        data: formData,
+        dataType: 'json',
+        success: function(data) {
+          console.log('success');
+          _addMovieRowToTable(data);
+        }
+      });
+    });
+  };
+
+  // take the response, populate the table with new movie
+  var _addMovieRowToTable = function(data) {
+    // TODO
+  };
 
   return exports;
 })();
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,22 @@ class MoviesController < ApplicationController
       format.json { render json: Movie.all }
     end
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      respond_to do |format|
+        format.json { render json: @movie, status: :created }
+      end
+    else
+      respond_to do |format|
+        format.json { render nothing: true, status: 400 }
+      end
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :release_date)
+    end
 end
