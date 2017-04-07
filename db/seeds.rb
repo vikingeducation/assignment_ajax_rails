e9209054f06bb4dee1e9e23d3381362187cd278c
Kenneth Kottenstette <kennykotten@gmail.com>
2017-04-06 18:08:21 -0700

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+I = 15
+
+I.times do |i|
+  title = "movie #{i}"
+
+  movie = Movie.create(title: title, release_date: Time.now)
+  Review.create(reviewer_name: "Bob  #{i}",
+                title: "Bobs Review  #{i}",
+                review_text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                review_date: Time.now,
+                movie_id: movie.id)
+end
