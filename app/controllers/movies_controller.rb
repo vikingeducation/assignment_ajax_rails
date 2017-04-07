bf4e79df22a449a526ac2ca32c714dc7a5b34f5e
Kenneth Kottenstette <kennykotten@gmail.com>
2017-04-06 19:29:53 -0700

    returning .json

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
 class MoviesController < ApplicationController
+  # respond_to :html, :js   # all actions covered
+  # respond_to :json, :only => [:create, :show]
   def index
     @movies = Movie.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @movies, status: 201 }
+    end
   end
-  
+
 end
